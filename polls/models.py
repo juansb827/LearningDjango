c516481fd2a0ce459bed346f5248ac92ec7cc830
Juan sebastian bedoya <juansb827@gmail.com>
2018-02-02 08:34:31 -0500

    first commit

---
+++ polls/models.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.forms import ModelForm
+from django import forms
+from django.contrib.auth.models import User
+
+
+
+# Create your models here.
+class Imagen(models.Model):
+    url = models.CharField(max_length=1000)
+    title = models.CharField(max_length=150,blank=True)
+    description= models.CharField(max_length=1000,null=True)
+    type = models.CharField(max_length=5,blank=True)
+    imageFile= models.ImageField(upload_to='images',null=True)
+    user = models.ForeignKey(User,  null=True)
+
+class ImageForm(ModelForm):
+
+    class Meta:
+        model = Imagen
+        fields = ['url', 'title', 'description','type','imageFile']
+
+class UserForm(ModelForm):
+        username= forms.CharField(max_length=50)
+        first_name=forms.CharField(max_length=20)
+        lastname_name = forms.CharField(max_length=20)
+        email = forms.EmailField()
+        password = forms.CharField(widget=forms.PasswordInput())
+        password2 = forms.CharField(widget=forms.PasswordInput())
+
+        class Meta:
+            model = User
+            fields = ['username','first_name','last_name','email','password','password2']
+
+        def clean_username(self):
+            username= self.cleaned_data['username']
+            if User.objects.filter(username=username):
+                raise forms.ValidationError('Ya registrado usuario')
+            return username
+
+        def clean_email(self):
+            email= self.cleaned_data['email']
+            if User.objects.filter(email=email):
+                raise   forms.ValidationError('Ya registrado mail ')
+            return email
+
+        def clean_password2(self):
+            password = self.cleaned_data['password']
+            password2 = self.cleaned_data['password2']
+            if password!=password2:
+                raise forms.ValidationError('Pass no coinciden')
+            return password2
+
+
+
+
+
+
+
+
---
+++ polls/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^add/$', views.add_image, name='addImage'),
+    url(r'^addUser/$', views.add_user, name='addUser'),
+    url(r'^login/$', views.login_view, name='login'),
+    url(r'^logout/$', views.logout_view, name='logout')
+
+]
