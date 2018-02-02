d27ddc8a4e19b1248dfe80b7c70fc4fb1cf5d099
Juan sebastian bedoya <juansb827@gmail.com>
2018-02-02 09:02:42 -0500

    parte 3

--- polls/views.py
+++ polls/views.py
@@ -72,7 +72,7 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect(reverse('image:index'))
+    return HttpResponseRedirect(reverse('images:index'))
 
 
 
